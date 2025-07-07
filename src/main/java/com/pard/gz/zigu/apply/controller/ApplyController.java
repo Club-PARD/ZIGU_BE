aa38409294bfdfd8b208ab652fc0715930a5ad02
SeoGyeongmi <22200360@handong.ac.kr>
2025-07-08 01:03:26 +0900

    아 이걸 까먹

--- src/main/java/com/pard/gz/zigu/apply/controller/ApplyController.java
+++ src/main/java/com/pard/gz/zigu/apply/controller/ApplyController.java
@@ -1,6 +1,8 @@
 package com.pard.gz.zigu.apply.controller;
 
 import com.pard.gz.zigu.apply.dto.ApplySaveReqDto;
+import com.pard.gz.zigu.apply.repository.ApplyRepo;
+import com.pard.gz.zigu.apply.service.ApplyService;
 import com.pard.gz.zigu.responseDto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/apply")
 public class ApplyController {
 
+    private final ApplyService applyService;
+
     @Operation(summary = "신청서 (작성 후)저장", description = "유저가 빌리고 싶은 물품의 대여 신청서 저장")
     @PostMapping("/save")
     public ResponseEntity<ApiResponse<Void>> saveApply(@RequestBody ApplySaveReqDto applySaveReqDto){
         // service
+        applyService.createApply(applySaveReqDto);
         return ResponseEntity.ok(new ApiResponse<>(200, true, "신청서 저장 성공", null));
     }
 }
