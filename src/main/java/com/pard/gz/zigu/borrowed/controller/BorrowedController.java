ac29f44d94b6d08e79bf22ac1d01b577fd195cd2
SeoGyeongmi <22200360@handong.ac.kr>
2025-07-10 14:25:49 +0900

    RequestParam Update

--- src/main/java/com/pard/gz/zigu/borrowed/controller/BorrowedController.java
+++ src/main/java/com/pard/gz/zigu/borrowed/controller/BorrowedController.java
@@ -20,21 +20,21 @@ public class BorrowedController {
 
     @Operation(summary = "빌린 내역 조회", description = "내가 빌리고 있는 물품들 조회")
     @GetMapping("/borrow")
-    public ResponseEntity<ApiResponse<List<BorrInfoResDto>>> getBorrowedList(Long userId){
+    public ResponseEntity<ApiResponse<List<BorrInfoResDto>>> getBorrowedList(@RequestParam Long userId){
         List<BorrInfoResDto> dtos = borrService.getBorrowedList(userId);
         return ResponseEntity.ok(ApiResponse.success(dtos));
     }
 
     @Operation(summary = "빌려 준 내역 조회", description = "내가 빌려주고 있는 물품들 조회")
     @GetMapping("/lend")
-    public ResponseEntity<ApiResponse<List<BorrInfoResDto>>> getLendList(Long userId){
+    public ResponseEntity<ApiResponse<List<BorrInfoResDto>>> getLendList(@RequestParam Long userId){
         List<BorrInfoResDto> dtos = borrService.getLendList(userId);
         return ResponseEntity.ok(ApiResponse.success(dtos));
     }
 
     @Operation(summary = "빌려 준 내역에서 반납 요청(반납 완료 버튼)", description = "현재 내역에서 대여 상태를 반납 완료로 변경")
     @PatchMapping("/return")
-    public ResponseEntity<ApiResponse<Void>> returnCompleted(Long borrowedId){
+    public ResponseEntity<ApiResponse<Void>> returnCompleted(@RequestParam Long borrowedId){
         borrService.returnCompleted(borrowedId);
         return ResponseEntity.ok(new ApiResponse<>(200, true, "반납 완료 성공", null));
     }
